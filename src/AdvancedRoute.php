43875ad3e2a0886674f184d3bf023a7a3c74fcfa
Milan Lesichkov <lesichkovm@gmail.com>
2016-11-06 08:35:59 +0000

    Create AdvancedRoute.php

---
+++ src/AdvancedRoute.php
@@ -0,0 +1,51 @@
+<?php
+
+class AdvancedRoute {
+
+    public static function controller($path, $controllerClassName) {
+        $class = new ReflectionClass('App\Http\Controllers\\' . $controllerClassName);
+        
+        $publicMethods = $class->getMethods(ReflectionMethod::IS_PUBLIC);
+        
+        foreach ($publicMethods as $method) {
+            $methodName = $method->name;
+            if ($methodName == 'getMiddleware') {
+                continue;
+            }
+            if (self::stringStartsWith($methodName, 'any')) {
+                $slug = self::slug($method);
+                //var_dump($slug);
+                Route::any($path . '/' . $slug, 'QuoteController@' . $methodName);
+            }
+            if (self::stringStartsWith($methodName, 'get')) {
+                $slug = self::slug($method);
+                //var_dump($slug);
+                Route::get($path . '/' . $slug, 'QuoteController@' . $methodName);
+            }
+            if (self::stringStartsWith($methodName, 'post')) {
+                $slug = self::slug($method);
+                //var_dump($slug);
+                Route::post($path . '/' . $slug, 'QuoteController@' . $methodName);
+            }
+        }
+        Route::get($path, 'QuoteController@anyIndex');
+    }
+
+    protected static function stringStartsWith($string, $match) {
+        return (substr($string, 0, strlen($match)) == $match) ? true : false;
+    }
+
+    protected static function slug($method) {
+        $methodName = $method->name;
+        $cleaned = str_replace(['any', 'get', 'post', 'delete'], '', $methodName);
+        $snaked = \Illuminate\Support\Str::snake($cleaned, ' ');
+        $slug = str_slug($snaked, '-');
+        foreach ($method->getParameters() as $parameter) {
+            if ($parameter->hasType()) {
+                continue;
+            }            
+            $slug .= sprintf('/{%s%s}', $parameter->getName(), $parameter->isDefaultValueAvailable() ? '?' : '');
+        }
+        return $slug;
+    }
+}
