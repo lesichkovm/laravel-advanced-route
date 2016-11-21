f311a1408f8ca0e3dd3d4487c21176d887e47123
Juan Antonio Orozco Bianchi <iam@mexcoder.com>
2016-11-21 00:37:06 -0600

    replaced getClass with a custom implementation of hasType
    getClass will try to autoload the class and if it cand find it it will throw an exception hastype will only test if it has a type or not based on the text representation of the reflectionParmeter see http://php.net/manual/en/reflectionparameter.getclass.php#108620 credit to "tom at r dot je"

--- src/AdvancedRoute.php
+++ src/AdvancedRoute.php
@@ -41,21 +41,28 @@ class AdvancedRoute {
         $cleaned = str_replace(['any', 'get', 'post', 'delete'], '', $methodName);
         $snaked = \Illuminate\Support\Str::snake($cleaned, ' ');
         $slug = str_slug($snaked, '-');
-        
+
         if($slug == "index")
             $slug = "";
         foreach ($method->getParameters() as $parameter) {
 
-            if ($parameter->getClass() != null) {
+            if (self::hasType($parameter)) {
                 continue;
             }
             $slug .= sprintf('/{%s%s}', $parameter->getName(), $parameter->isDefaultValueAvailable() ? '?' : '');
         }
-        
-        if($slug != null && $slug[0] == '/')
+
+        if($slug != null && $slug[0] ==  '/')
             return substr($slug,1);
-        
+
         return $slug;
+    }
+
+    protected static function hasType(ReflectionParameter $param) {
+        //TODO: if php7 use the native method
+
+        preg_match('/\[\s\<\w+?>\s([\w]+)/s', $param->__toString(), $matches);
+        return isset($matches[1]) ? true : false;
     }
 
 }
