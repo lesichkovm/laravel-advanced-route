73d26cf1ed4f7cc96335407d2d1f31ef73013924
MILAN <MILAN@sinevia>
2017-02-09 03:24:43 +0000

    Clean up

--- src/AdvancedRoute.php
+++ src/AdvancedRoute.php
@@ -30,11 +30,12 @@ class AdvancedRoute {
         // as the anyIndex will be will be called instead.
         $methods = [];
         foreach ($publicMethods as $method) {
-            if ($method->name == 'getMiddleware')
+            if ($method->name == 'getMiddleware') {
                 continue;
+            }
 
             $method->slug = self::slug($method);
-            $methods[] = $method; 
+            $methods[] = $method;
         }
 
         // Sort the routes so that the routes without any parameters, one with no {, come first.
@@ -42,12 +43,13 @@ class AdvancedRoute {
             $aHasParam = false !== strpos($a->slug, '{');
             $bHasParam = false !== strpos($b->slug, '{');
 
-            if (!$aHasParam && $bHasParam)
+            if (!$aHasParam && $bHasParam) {
                 return -1;
-            if (!$bHasParam && $aHasParam)
+            }
+            if (!$bHasParam && $aHasParam) {
                 return 1;
+            }
             return (strcmp($a->slug, $b->slug));
-
         });
 
         foreach ($methods as $method) {
@@ -55,8 +57,7 @@ class AdvancedRoute {
             $methodName = $method->name;
 
             $httpMethod = null;
-            foreach(self::$httpMethods as $httpMethod)
-            {
+            foreach (self::$httpMethods as $httpMethod) {
                 if (self::stringStartsWith($methodName, $httpMethod)) {
                     Route::$httpMethod($path . '/' . $slug, $controllerClassName . '@' . $methodName);
 
@@ -71,22 +72,7 @@ class AdvancedRoute {
         }
 
         if (self::EMIT_ROUTE_STATEMENTS) {
-            // Compute max length of the "prefix" of each route command so that when we issue the strings
-            // we can make all the 2nd parameters line up verticaly.  Easier to read and thus easier to check
-            // to ensure we are emitting the right routes.
-            $maxPrefixLen = 0;
-            array_walk($routes, function($route) use (&$maxPrefixLen) {
-                $l = strlen($route->prefix);
-                if ($l > $maxPrefixLen)
-                    $maxPrefixLen = $l;
-            });
-
-            if (!is_dir('/tmp/controllerRoutes'))
-                mkdir('/tmp/controllerRoutes');
-            $routeList = sprintf("<?php\n// %s \"Controller\" Routes\n", $controllerClassName);
-            foreach ($routes as $route)
-                $routeList .= sprintf("%-{$maxPrefixLen}s '%s');\n", $route->prefix, $route->target);
-            file_put_contents("/tmp/controllerRoutes/{$controllerClassName}.php", $routeList . PHP_EOL);
+            self::emitRoutes($routes);
         }
     }
 
@@ -99,25 +85,28 @@ class AdvancedRoute {
         // they appear in the string, so for example, getCompanyname, becomes "compname" instead of companyname because "any"
         // is removed from the word company in addition to the removal of "get" from the front of the string. Use a preg
         // to anchor the search to the front of the method name.
-        if (!self::$methodNameAtStartOfStringPattern)
-            self::$methodNameAtStartOfStringPattern = '/^('.implode('|', self::$httpMethods).')/';
+        if (!self::$methodNameAtStartOfStringPattern) {
+            self::$methodNameAtStartOfStringPattern = '/^(' . implode('|', self::$httpMethods) . ')/';
+        }
 
         $cleaned = preg_replace(self::$methodNameAtStartOfStringPattern, '', $method->name);
         $snaked = \Illuminate\Support\Str::snake($cleaned, ' ');
         $slug = str_slug($snaked, '-');
 
-        if($slug == "index")
+        if ($slug == "index") {
             $slug = "";
-        foreach ($method->getParameters() as $parameter) {
+        }
 
+        foreach ($method->getParameters() as $parameter) {
             if (self::hasType($parameter)) {
                 continue;
             }
             $slug .= sprintf('/{%s%s}', strtolower($parameter->getName()), $parameter->isDefaultValueAvailable() ? '?' : '');
         }
 
-        if($slug != null && $slug[0] ==  '/')
-            return substr($slug,1);
+        if ($slug != null && $slug[0] == '/') {
+            return substr($slug, 1);
+        }
 
         return $slug;
     }
@@ -127,6 +116,30 @@ class AdvancedRoute {
 
         preg_match('/\[\s\<\w+?>\s([\w]+)/s', $param->__toString(), $matches);
         return isset($matches[1]) ? true : false;
+    }
+
+    protected static function emitRoutes($routes) {
+        // Compute max length of the "prefix" of each route command so that when we issue the strings
+        // we can make all the 2nd parameters line up verticaly.  Easier to read and thus easier to check
+        // to ensure we are emitting the right routes.
+        $maxPrefixLen = 0;
+        array_walk($routes, function($route) use (&$maxPrefixLen) {
+            $l = strlen($route->prefix);
+            if ($l > $maxPrefixLen) {
+                $maxPrefixLen = $l;
+            }
+        });
+
+        if (!is_dir('/tmp/controllerRoutes')) {
+            mkdir('/tmp/controllerRoutes');
+        }
+
+        $routeList = sprintf("<?php\n// %s \"Controller\" Routes\n", $controllerClassName);
+        foreach ($routes as $route) {
+            $routeList .= sprintf("%-{$maxPrefixLen}s '%s');\n", $route->prefix, $route->target);
+        }
+
+        file_put_contents("/tmp/controllerRoutes/{$controllerClassName}.php", $routeList . PHP_EOL);
     }
 
 }
